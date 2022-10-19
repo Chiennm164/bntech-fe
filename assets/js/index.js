45676573ec047d61579372845d45430ff3060027
Chiennm164 <84827688+Chiennm164@users.noreply.github.com>
2022-10-19 17:57:39 +0700

    push

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,30 @@
-// const $ = document.querySelector.bind(document);
-// const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-// const imgProducts = [
-//      'https://kmmfilm.vn/wp-content/uploads/2020/07/T-424x225.png',
-//      'https://kmmfilm.vn/wp-content/uploads/2020/07/T-424x225.png',
-//      'https://kmmfilm.vn/wp-content/uploads/2020/07/T-424x225.png'
-// ]
+let elNav = $('#navbar')
+let elMenu = $('.menu-mobile')
 
-// runText()
-// handleProducts()
-// function runText() {
-//      let textRun = $('#text-run p')
-//      console.log(textRun);
-// }
-// function handleProducts() {
-//      let div = $('#products .row')
-//      // console.log(img);
+elMenu.onclick = () => {
+     console.log('a');
+   let a=  elNav.classList.toggle('show')
 
-//    let htmls=imgProducts.map((item) => {
-//       return    `<div class="col ">
-//                 <div class="project">
-//                     <img src="${item}" alt="">
-//                 </div>
-//             </div>`
-//      }).join('')
-//      console.log(htmls);
-//      div.innerHTML = htmls
-
-// }
+     if (a) {
+          elMenu.innerHTML = `
+        <span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+       </svg>
+     </span>
+           `
+     } else {
+          elMenu.innerHTML = `
+          <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+             </svg>
+     </span>
+          `     }
+}
 
 const duAn = document.querySelector("#du-an");
 const congNghiep = document.querySelector("#cong-nghiep");
@@ -37,32 +34,32 @@ const rowDuAn = document.querySelector(".row-du-an");
 const rowCongNghiep = document.querySelector(".row-cong-nghiep");
 const rowDieuHoa = document.querySelector(".row-dieu-hoa");
 
-function handleTitle(type){
-     if(type === "duAn"){
+function handleTitle(type) {
+     if (type === "duAn") {
           duAn.style.color = "#ecec82";
           dieuHoa.style.color = "#243f99";
           congNghiep.style.color = "#243f99";
 
-          rowDuAn.style.display="flex";
-          rowDieuHoa.style.display="none";
-          rowCongNghiep.style.display="none";
+          rowDuAn.style.display = "flex";
+          rowDieuHoa.style.display = "none";
+          rowCongNghiep.style.display = "none";
 
-     }else  if(type === "congNghiep"){
+     } else if (type === "congNghiep") {
           duAn.style.color = "#243f99";
           dieuHoa.style.color = "#243f99";
           congNghiep.style.color = "#ecec82";
 
-          rowDuAn.style.display="none";
-          rowDieuHoa.style.display="none";
-          rowCongNghiep.style.display="block";
-     }else  if(type === "dieuHoa"){
+          rowDuAn.style.display = "none";
+          rowDieuHoa.style.display = "none";
+          rowCongNghiep.style.display = "block";
+     } else if (type === "dieuHoa") {
           duAn.style.color = "#243f99";
           dieuHoa.style.color = "#ecec82";
           congNghiep.style.color = "#243f99";
 
-          rowDuAn.style.display="none";
-          rowDieuHoa.style.display="block";
-          rowCongNghiep.style.display="none";
+          rowDuAn.style.display = "none";
+          rowDieuHoa.style.display = "block";
+          rowCongNghiep.style.display = "none";
 
      }
 
